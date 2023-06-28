3f0dd0bd9aa7350568659a27ef4b9abbdef05585
LaysLeticia <136758166+LaysLeticia@users.noreply.github.com>
2023-06-28 15:13:28 -0300

    Add files via upload
    Atividade 28/06

---
+++ Untitled-1-atv.js
@@ -0,0 +1,20 @@
+//ATIVIDADE
+//Questao 1
+var numero1 = new Number (300);
+console.log(numero1);
+var numero2 = new Number (999);
+console.log(numero2);
+var numero3 = new Number (21);
+console.log(numero3);
+
+//Questao 2 
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.EPSILON);
+console.log(Number.MIN_SAFE_INTEGER);
+
+//Questao 3
+var numero4 = new Number(150);
+console.log(numero4);
+console.log(numero4.toString());
+
